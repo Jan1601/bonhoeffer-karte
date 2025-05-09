3b175c749df20b4e5d7f5958d40e0c4a74cca37c
Jan1601 <jkgwieting@gmail.com>
2025-05-09 09:17:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,51 +13,31 @@ const links = "<br><a href='https://www.britannica.com/biography/Dietrich-Bonhoe
 var locations = [
     {
         name: "Berlin", coords: [52.5200, 13.4050], info:
-        "Dietrich Bonhoeffer verbrachte von 1912 bis 1923 eine prägende Zeit in Berlin..." + links
+        "Dietrich Bonhoeffer verbrachte von 1912 bis 1923 eine prägende Zeit in Berlin, die sein Denken stark beeinflusste. In dieser Zeit studierte er Theologie an der Universität Berlin. Bonhoeffer war in Berlin in einer Zeit, in der es politische und soziale Veränderungen gab. In Berlin lernte er die Arbeiten bedeutender Theologen kennen, wie die von Karl Barth, deren Betonung auf der Transzendenz Gottes lag. Er entwickelte eine kritische Haltung gegenüber einer allzu weltlichen oder verweltlichten Kirche, die seiner Ansicht nach ihre prophetische Rolle verloren hatte. Diese Zeit in Berlin war ein großer Schritt in die Richtung seiner späteren Haltung zur Kirche im Widerstand gegen den Nationalsozialismus. Bonhoeffer erlebte die Kirche als zunehmend gleichgültig gegenüber den sozialen und politischen Fragen der Zeit. Bonhoeffer reagierte darauf damit, dass er eine Theologie entwickelte, die das Leben des Christen in der Welt und die Verantwortung der Kirche für die Gesellschaft stärker betonte. Dies wurde zu einem zentralen Thema in seinen späteren Werken und prägte seine Idee von einer aktiven Kirche." + links
     },
     {
         name: "Breslau", coords: [51.1079, 17.0385], info:
-        "Dietrich Bonhoeffer wurde am 4. Februar 1906 in Breslau geboren..." + links
+        "Dietrich Bonhoeffer wurde am 4. Februar 1906 in Breslau geboren. Er hatte insgesamt acht Geschwister. Sein Vater Karl Bonhoeffer war Psychiater und Neurologe. Seine Mutter Paula Bonhoeffer war Lehrerin. Dietrichs Mutter hat die Kinder in den ersten Jahren zu Hause unterrichtet. Dabei hat sie auf eine christliche Erziehung geachtet, obwohl Dietrichs Vater nicht religiös war. Daher war sie auch nur selten in Gottesdiensten." + links
     },
     {
         name: "Flossenbürg", coords: [49.7325, 12.3478], info:
-        "Nachdem Dietrich Bonhoeffer am 5. April 1943 von der Gestapo verhaftet wurde..." + links
+        "Nachdem Dietrich Bonhoeffer am 5. April 1943 von der Gestapo verhaftet wurde, wurde er im Berliner Gefängnis Tegel inhaftiert. Im Februar 1945 wurde Bonhoeffer dann in das Konzentrationslager Buchenwald und darauf in das KZ Flossenbürg gebracht. In Flossenbürg wurde er, aufgrund des gescheiterten Attentats vom 20. Juli 1944, bei dem ein Umsturz des Regimes geplant war, als Mitverschwörer angeklagt. Am 8. April 1945 wurde er von einem Standgericht zum Tode verurteilt. Am 9. April 1945 wurde er im Alter von 39 Jahren erhängt. Nur wenige Wochen später wurde das Lager befreit. Während seiner Zeit im Gefängnis sind die bedeutendsten theologischen Werke Bonhoeffers: 'Widerstand und Ergebung' und 'Ethik'. Diese Schriften sind von großer theologischer Bedeutung und spiegeln seine Auseinandersetzung mit dem nationalsozialistischen Regime sowie seine tiefen Überlegungen über Glaube, Ethik und das Leben als Christ in einer Zeit der Krise." + links
     },
     {
         name: "Tübingen", coords: [48.5216, 9.0576], info:
-        "Kurzer Forschungsaufenthalt (1931/32): Nach seiner Rückkehr aus New York..." + links
-    },
-    {
-        name: "London", coords: [51.5074, -0.1278], info:
-        "Auch wenn Bonhoeffer keinen bedeutenden, dokumentierten Aufenthalt in London hatte..." + links
-    },
-    {
-        name: "New York", coords: [40.7128, -74.0060], info:
-        "Studium an der Union Theological Seminary (1930–1931): Bonhoeffer kam 1930 als Stipendiat..." + links
-    },
-    {
-        name: "Finkenwalde", coords: [53.4285, 14.5528], info:
-        "Dietrich Bonhoeffer hielt Gemeinschaft, Beichte und die Nachfolge Christi für sehr wichtig..." + links
-    },
-    {
-        name: "Stockholm", coords: [59.3293, 18.0686], info:
-        "Bonhoeffer war im Mai und Juni 1942 in Stockholm, um sich dort mit George Bell zu treffen..." + links
-    },
-    {
-        name: "Oslo", coords: [59.9139, 10.7522], info:
-        "In Norwegen setzte sich Bonhoeffer auch intensiver mit der Frage auseinander..." + links
-    },
-    {
-        name: "Madrid", coords: [40.4168, -3.7038], info:
-        "Spanien: Im Jahr 1928 verbrachte Dietrich Bonhoeffer einige Zeit in Barcelona..." + links
+        "Kurzer Forschungsaufenthalt (1931/32): Nach seiner Rückkehr aus New York war Bonhoeffer für einige Zeit in Tübingen. Dort arbeitete er als Privatdozent für Theologie, hielt aber keine regulären Vorlesungen. In dieser Phase setzte er seine theologischen Forschungen fort. Verbindungen zur Theologischen Fakultät: Während seiner akademischen Laufbahn stand Bonhoeffer in Kontakt mit verschiedenen Tübinger Theologen. Die Universität war ein wichtiges Zentrum der evangelischen Theologie in Deutschland. Überbleibsel seines Denkens in Tübingen: Nach dem Zweiten Weltkrieg wurde Bonhoeffers Theologie besonders in Tübingen intensiv rezipiert. Jürgen Moltmann, einer der bekanntesten Tübinger Theologen, griff viele seiner Gedanken in der Theologie der Hoffnung auf. Die Universität Tübingen hat bis heute eine enge Verbindung zu Bonhoeffers theologischen Ideen. Auswirkungen: Bonhoeffers Aufenthalt in Tübingen hat sein Denken nicht unbedingt geprägt, sondern sorgte eher dafür, dass sein Denken dokumentiert wurde und in Erinnerung blieb." + links
     },
     {
         name: "Zürich", coords: [47.3769, 8.5417], info:
-        "Theologische Kontakte und Austausch: Bonhoeffer pflegte enge Beziehungen zu reformierten Theologen in der Schweiz..." + links
+        "Theologische Kontakte und Austausch: Bonhoeffer pflegte enge Beziehungen zu reformierten Theologen in der Schweiz. Während seiner Aufenthalte in Zürich und Genf kam Bonhoeffer regelmäßig mit anderen Theologen zusammen, um sich über aktuelle theologische und kirchenpolitische Fragen auszutauschen. Diese Gespräche waren für Bonhoeffer besonders wertvoll, da sie ihm halfen, trotz der Isolation in Deutschland im internationalen theologischen Diskurs eingebunden zu bleiben. Geheime Missionen und Widerstandstätigkeit: Neben dem theologischen Austausch war Bonhoeffer auch in geheimer Mission für die deutsche Abwehr tätig. Sein Schwager Hans von Dohnanyi und der Abwehroffizier Hans Oster integrierten ihn in die Spionageabteilung, um ihn vor der Gestapo zu schützen. In dieser Funktion reiste Bonhoeffer 1941 und 1942 mehrfach in die Schweiz, um Verbindungen zu den Alliierten zu knüpfen. Ziel war es, herauszufinden, ob die westlichen Mächte bereit wären, mit einem nicht-nationalsozialistischen Deutschland Friedensverhandlungen aufzunehmen. Eine besonders riskante Aktion war das sogenannte 'Unternehmen Sieben', bei dem Bonhoeffer gemeinsam mit Dohnanyi und Friedrich Justus Perels versuchte, jüdischen Mitbürgern die Flucht in die Schweiz zu ermöglichen. Im Herbst 1942 gelang es 14 Personen in die Schweiz zu bringen. Allerdings wurde der Geldtransfer zur Unterstützung der Flüchtlinge entdeckt, was letztlich zur Verhaftung Bonhoeffers am 5. April 1943 führte. Einflüsse auf Bonhoeffers Theologie: Durch den engen Kontakt zum Ökumenischen Rat der Kirchen in Genf wurde Bonhoeffer klar, wie wichtig eine weltweite, verbindliche Gemeinschaft der Kirchen ist (nicht nur theologisch, sondern auch politisch). Daraus entstand seine Vorstellung der Kirche als 'Kirche für andere', die nicht nur innerlich fromm, sondern öffentlich verantwortlich handelt. In Zürich und Basel vertiefte Bonhoeffer den Austausch mit Karl Barth, dessen Theologie ihn schon früh geprägt hatte. Die direkte Auseinandersetzung mit Barth bestärkte Bonhoeffer darin, dass Theologie immer in den konkreten geschichtlichen Kontext hineingesprochen sein muss, eine Haltung, die besonders in seiner 'Ethik' und seinen Briefen aus der Haft sichtbar wird. In den späteren Jahren, insbesondere in den 'Briefen und Aufzeichnungen aus der Haft', reflektiert Bonhoeffer über ein 'religionsloses Christentum'. Auch das hat Wurzeln in den offenen, oft liberaleren Debatten, die er mit Theologen in der Schweiz führte. Sein Denken wurde freier, radikaler, weniger an kirchlichen Institutionen orientiert. Die Ausflüge in die Schweiz bestärkten Bonhoeffer in seinem Verständnis von Kirche als politisch verantwortlicher Gemeinschaft, förderten seine Abkehr von dogmatischem Denken zugunsten einer 'diesseitigen', weltzugewandten Theologie und machten ihn zum Theologen des Widerstands. Was bleibt: Obwohl Bonhoeffers Aufenthalte in der Schweiz von großer Bedeutung waren, gibt es in der Schweiz kaum öffentliche Erinnerungsorte an ihn. Dennoch bleibt sein Wirken durch Veranstaltungen und Veröffentlichungen präsent und inspiriert weiterhin viele Menschen." + links
+    },
+    {
+        name: "Madrid", coords: [40.4168, -3.7038], info:
+        "Spanien: Im Jahr 1928 verbrachte Dietrich Bonhoeffer einige Zeit in Barcelona, während eines längeren Studienaufenthalts in Europa. Dort lernte er Spanisch und setzte sich mit den theologischen Strömungen der Zeit auseinander. Barcelona bot ihm einen Einblick in die Vielfalt des Christentums und die sozialen und politischen Herausforderungen der Region. Das regte Bonhoeffer zu theologischen und politischen Reflexionen an. Der Aufenthalt beeinflusste seine Haltung zur Verantwortung der Kirche in einer politisierten Welt. Bonhoeffer begann, über die Rolle von Christen im Widerstand gegen autoritäre Regime nachzudenken. Auch wenn sein Aufenthalt in Barcelona nur kurz war, erweiterte er Bonhoeffers Meinungsbild und prägte seine spätere Arbeit, vor allem seine Vorstellung von der universellen Kirche und der christlichen Verantwortung in schwierigen Zeiten. Barcelona bleibt also eine bedeutende Station in seiner geistigen und theologischen Entwicklung." + links
     }
 ];
 
-// Marker für alle Städte hinzufügen
+// Marker setzen
 locations.forEach(function(location) {
     var marker = L.marker(location.coords).addTo(map);
     marker.bindPopup("<b>" + location.name + "</b><br>" + location.info);
